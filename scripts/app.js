d0d6434a6efaa840d1db0519f03469b08aef08e0
Lucas <llgondariz@gmail.com>
2025-06-22 06:15:12 -0300

    Renderizo productos desde la base en la grilla, terminando de armar funciones como renderProducts, que hacen uso de getProducts y createProductCard

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,21 @@
 const API_TOKEN = 'patzBjehHGfd2FzO9.9aabe9349892c530f40039288659ea2b64218bb11680cc51c7fa0efb7517f7a4';
 const BASE_ID = 'appSPKO7TLX1jBPct';
 const TABLE_NAME = 'products';
-const proxy = 'https://cors-anywhere.herokuapp.com/';
 const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`; // Se usa $ y no concat para que sea más legible - Tambien los backticks `` son como las comillas pero permiten interpolación de variables
 
-/*
-fetch(API_URL, { 
-    headers: {'Authorization': `Bearer ${API_TOKEN}`} 
-})
-.then(res => console.log(res.ok 
-    ? 'Autorizado. El token y la URL son correctos.' 
-    : `NO Autorizado. Revisa tus datos. (Error: ${res.status})`
-));*/
+// Funcion TEST que trae la tabla por consola
+const getProductsTEST = async () => {
+    const response = await fetch(API_URL, {
+        headers: {
+            'Authorization': `Bearer ${API_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response.json(); // El método json() convierte la respuesta en un objeto JS
+    console.log('data', data);
+}
+
+getProductsTEST();
 
 const getProducts = async () => {
     const response = await fetch(API_URL, {
@@ -21,34 +25,33 @@ const getProducts = async () => {
         }
     });
     const data = await response.json(); // El método json() convierte la respuesta en un objeto JS
-    console.log('data', data);
+    return data.records;
 } 
-getProducts();
 
-/* 
+
 // Debo crear las tarjetas e insertarlas en el DOM
 const grid = document.querySelector('.catalogo'); // Elemento padre con todos los productos
 
-// forma clasita de crear la funcion
+// forma clasica de crear la funcion, otra forma, forma de funcion lambda (product) => {}
 function createProductCard(product){ 
     const card = document.createElement('article');
     card.classList.add('producto');
 
     const img = document.createElement('img');
-    img.src = product.image;
-    img.alt = product.name;
+    img.src = product.fields.img;
+    img.alt = product.fields.name;
 
     const div = document.createElement('div');
     div.classList.add('infoProducto');
 
     const title = document.createElement('h2');
-    title.textContent = product.name;
+    title.textContent = product.fields.name;
 
     const description = document.createElement('p');
-    description.textContent = product.description;
+    description.textContent = product.fields.description;
 
     const price = document.createElement('p');
-    price.textContent = `Precio:$${product.price}`; //probar si funciona
+    price.textContent = `Precio:$${product.fields.price}`; //probar si funciona
 
     const form = document.createElement('form');
     form.classList.add('contenedorMenu');
@@ -62,7 +65,7 @@ function createProductCard(product){
     const btnDisminuir = document.createElement('button');
     btnDisminuir.type = 'button';
     btnDisminuir.id = 'contenedorSecundario1Disminuir'; // PUEDE SER QUE NECESITE COMILLAS O ESTE MAL
-    btnDisminuir.textContent = '▲';
+    btnDisminuir.textContent = '▼';
 
     const input = document.createElement('input');
     input.type = 'number';
@@ -79,7 +82,7 @@ function createProductCard(product){
     div.appendChild(title);
     div.appendChild(description);
     div.appendChild(price);
-    card.appendChild(form);
+    div.appendChild(form);
     form.appendChild(btnAumentar);
     form.appendChild(btnDisminuir);
     form.appendChild(input);
@@ -88,11 +91,12 @@ function createProductCard(product){
     return card;
 } 
 
-//otra forma, forma de funcion lambda (product) => {}
+const renderProducts = async () =>{
+    const list = await getProducts();
+    list.forEach(product => {
+        const card = createProductCard(product);
+        grid.appendChild(card);
+    })
+}
 
-
-products.forEach(product => {
-    const card = createProductCard(product);
-    grid.appendChild(card);
-}); 
-*/
+renderProducts();
