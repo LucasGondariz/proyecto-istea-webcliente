fe7a59dadc6b89eb783acf4767d18d87fc4b47ee
Lucas <llgondariz@gmail.com>
2025-06-23 03:21:19 -0300

    Implemento sistema de logeo con login.html / .js / .css y tambien creo archivos aun sin editar para el crud

---
+++ scripts/login.js
@@ -0,0 +1,29 @@
+const API_TOKEN = 'patzBjehHGfd2FzO9.9aabe9349892c530f40039288659ea2b64218bb11680cc51c7fa0efb7517f7a4';
+const BASE_ID = 'appSPKO7TLX1jBPct';
+const TABLE_NAME = 'products';
+const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`; // Se usa $ y no concat para que sea más legible - Tambien los backticks `` son como las comillas pero permiten interpolación de variables
+
+import * as app from './app.js';
+
+const botonIngresar = document.getElementById('botonIngresar');
+botonIngresar.addEventListener('click', (event) => {
+    console.log('Botón ingresar presionado');
+    //event.preventDefault(); // Evita que el formulario se envíe y recargue la página
+    login();
+});
+
+
+function login(){
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (email === 'admin' && password === 'admin') {
+        console.log('Login exitoso');
+        windows.location.href = 'crud.html';
+    }
+    else {
+        console.log('Login fallido');
+        alert('Usuario o contraseña incorrectos');
+        return;
+    }
+}
